2d180eedf1cec556360ad2eeaa913f90bf0ae4b7
Sashafimoz <av812349@gmail.com>
2025-06-24 17:15:37 +0400

    tdggxg

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
-app.use(express.static('dist'))
+app.get('/', function (request, response) {
+    let html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>sfhhdhsddg</div>
+    <div>${Math.random()}</div>
+</body>
+</html>`
+    response.set({ 'Content-type': 'text/html' })
+    response.send(html)
+})
+// { num1: 12, num2: 78} ->90
+app.post('/calc-sum', function (req, res) {
+ const sum = req.body.num1 + req.body.num2  
+    res.status(200).json(sum)
+})
 app.listen(3000, () => console.log('сервер запущен'))
